111d50e691516086343a4fbfb22e05f08ffeceaa
NotZeetaa <rodrigo2005contente@gmail.com>
2022-08-11 18:54:25 +0100

    lmi: Fluid bringup
    Signed-off-by: NotZeetaa <rodrigo2005contente@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,7 +5,7 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_lmi.mk
+    $(LOCAL_DIR)/fluid_lmi.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_lmi-userdebug
+    fluid_lmi-userdebug
--- fluid_lmi.mk
+++ fluid_lmi.mk
@@ -8,13 +8,13 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common PixelOs stuff
-$(call inherit-product, vendor/aosp/config/common.mk)
+# Inherit some common Fluid stuff
+$(call inherit-product, vendor/fluid/config/common.mk)
 
 # Inherit from lmi device
 $(call inherit-product, device/xiaomi/lmi/device.mk)
 
-PRODUCT_NAME := aosp_lmi
+PRODUCT_NAME := fluid_lmi
 PRODUCT_DEVICE := lmi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
@@ -23,7 +23,8 @@ TARGET_SUPPORTS_QUICK_TAP := true
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
 TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_INCLUDE_GAPPS := true
+TARGET_INCLUDE_PIXELEXTRAS := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
